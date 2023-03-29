c58b58e8c20456b5834a44b9aa1bdaaf19041e02
Hightower-Agency <127230856+Hightower-Agency@users.noreply.github.com>
2023-03-29 13:11:09 -0400

    1:10pm, March 29th

--- js/script.js
+++ js/script.js
@@ -8,9 +8,91 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+Star Trek Quotes
 ***/
+var quotes = [
 
+  {
+      quote: 'Live now; make now always the most precious time. Now will never come again.',
+      source: 'Captain Jean-Luc Picard',
+      citation: 'Star Trek:TNG',
+      year: 'Stardate 45944.1'
+  },
+  {
+    quote: 'You know the greatest danger facing us is ourselves, and irrational fear of the unknown. There is no such thing as the unknown. Only things temporarily hidden, temporarily not understood.',
+    source: 'Captain James Tiberius Kirk',
+    citation: 'Star Trek:TOS',
+    year: 'Stardate 1512.2'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+/*** 
+Star Wars Quotes
+***/
+{
+  quote: 'Try not. Do or do not. There is no try.',
+  source: 'Yoda',
+  citation: 'Star Wars:The Empire Strikes Back',
+  year: '1980'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+/*** 
+Rick and Morty Quotes
+***/
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+/*** 
+Hitchhikers Guide to the Galaxy Quotes
+***/
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
+{
+  quote: 'Logic clearly dictates that the needs of the many outweigh the needs of the few...or the one.',
+  source: 'Commander Spock',
+  citation: 'Star Trek:The Wrath of Khan',
+  year: 'Stardate 8130.3'
+},
 
 
 /***
